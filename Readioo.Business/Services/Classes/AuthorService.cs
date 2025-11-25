e48ba46697fedc3d00b7966d1deeb83ec04e3c5c
Karim <Karim88999@gmail.com>
2025-11-25 18:29:48 +0200

    Author Added

--- Readioo.Business/Services/Classes/AuthorService.cs
+++ Readioo.Business/Services/Classes/AuthorService.cs
@@ -1,5 +1,7 @@
 ﻿using Demo.DataAccess.Repositories.UoW;
+using Microsoft.EntityFrameworkCore;
 using Readioo.Business.DataTransferObjects.Author;
+using Readioo.Business.DataTransferObjects.Book;
 using Readioo.Business.Services.Interfaces;
 using Readioo.Models;
 using System;
@@ -18,6 +20,56 @@ namespace Readioo.Business.Services.Classes
             _unitOfWork = unitOfWork;
         }
 
+        public AuthorDto getAuthorById(int id)
+        {
+
+            // 1. Get the Author entity
+            var author = _unitOfWork.AuthorRepository.GetById(id);
+            if (author == null)
+            {
+                return null;
+            }
+
+            // 2. Fetch the books for this author and map to BookDto
+            // We use the BookRepository to query books by AuthorId
+            var authorBooks = _unitOfWork.BookRepository.GetAll()
+                .Where(b => b.AuthorId == id)
+                .Select(b => new BookDto
+                {
+                    BookId = b.Id,
+                    Title = b.Title,
+                    Isbn = b.Isbn,
+                    Language = b.Language,
+                    AuthorId = b.AuthorId,
+                    PagesCount = b.PagesCount,
+                    PublishDate = b.PublishDate,
+                    MainCharacters = b.MainCharacters,
+                    Rate = b.Rate,
+                    Description = b.Description,
+                    BookImage = b.BookImage
+                    // Add other properties if needed by your view
+                })
+                .ToList();
+
+            // 3. Create the AuthorDto and assign the books
+            AuthorDto authorDto = new AuthorDto()
+            {
+                AuthorId = author.Id,
+                FullName = author.FullName,
+                Bio = author.Bio,
+                BirthCountry = author.BirthCountry,
+                BirthCity = author.BirthCity,
+                BirthDate = author.BirthDate,
+                DeathDate = author.DeathDate,
+                AuthorImage = author.AuthorImage,
+                Books = authorBooks // Pass the list here
+            };
+
+            return authorDto;
+        }
+
+        
+
         public IEnumerable<AuthorDto> getAllAuthors()
         {
             var Authors = _unitOfWork.AuthorRepository.GetAll().
@@ -32,7 +84,7 @@ namespace Readioo.Business.Services.Classes
                     DeathDate = a.DeathDate,
                     AuthorImage = a.AuthorImage
                 });
-                
+
 
             return Authors;
         }
@@ -58,6 +110,41 @@ namespace Readioo.Business.Services.Classes
             await _unitOfWork.CommitAsync();
         }
 
+        public async Task UpdateAuthor(int id, AuthorCreatedDto authorDto)
+        {
+            // Find the existing author
+            var existingAuthor = _unitOfWork.AuthorRepository.GetById(id);
 
+            if (existingAuthor == null)
+            {
+                throw new KeyNotFoundException($"Author with ID {id} not found.");
+            }
+
+            // Update properties
+            existingAuthor.FullName = authorDto.FullName;
+            existingAuthor.Bio = authorDto.Bio;
+            existingAuthor.BirthCity = authorDto.BirthCity;
+            existingAuthor.BirthCountry = authorDto.BirthCountry;
+            existingAuthor.BirthDate = authorDto.BirthDate;
+            existingAuthor.DeathDate = authorDto.DeathDate;
+            existingAuthor.AuthorImage = authorDto.AuthorImage;
+
+            // Save changes
+            _unitOfWork.AuthorRepository.Update(existingAuthor);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task DeleteAuthor(int id)
+        {
+            var existingAuthor = _unitOfWork.AuthorRepository.GetById(id);
+
+            if (existingAuthor == null)
+            {
+                throw new KeyNotFoundException($"Author with ID {id} not found.");
+            }
+
+            _unitOfWork.AuthorRepository.Remove(existingAuthor);
+            await _unitOfWork.CommitAsync();
+        }
     }
 }
